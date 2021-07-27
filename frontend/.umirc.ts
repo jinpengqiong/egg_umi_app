26aaabfab19e6389db6d4066d9ec7caf444a6e8b
Peng.Jin <peng.jin@symbio.com>
2021-07-27 15:19:41 +0800

    ok

--- frontend/.umirc.ts
+++ frontend/.umirc.ts
@@ -5,7 +5,27 @@ export default defineConfig({
     type: 'none',
   },
   routes: [
-    { path: '/', component: '@/pages/index' },
+    {
+      path: '/',
+      component: '@/layouts/index',
+      routes: [
+        {
+          path: '/',
+          component: './home/index.tsx',
+          title:'首页'
+        },
+        {
+          path: '/order',
+          component: './order/index.tsx',
+          title:'订单'
+        },
+        {
+          path: '/user',
+          component: './user/index.tsx',
+          title:'用户'
+        },
+      ]
+    },
   ],
   fastRefresh: {},
 });
---
+++ frontend/src/pages/home/index.tsx
@@ -0,0 +1,17 @@
+import React, {useState, useEffect} from 'react';
+
+interface propsType {
+  a: string
+}
+
+export default function (props: propsType) {
+  const [state, setState ] = useState()
+  useEffect(() => {
+
+  },[])
+  return (
+    <div>
+      Home Page
+    </div>
+  )
+}
