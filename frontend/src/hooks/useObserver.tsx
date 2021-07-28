1e3dae5ed80ebe5dba894eccbf8cac934ee0940c
Peng.Jin <peng.jin@symbio.com>
2021-07-28 17:30:00 +0800

    ok

--- frontend/src/hooks/useObserver.tsx
+++ frontend/src/hooks/useObserver.tsx
@@ -1,24 +1,26 @@
 import React, { useEffect } from 'react'
-
 interface Observer {
   unobserve: Function,
-  disconnect: Function
+  disconnect: Function,
+  observe: Function
 }
 
 let observer: Observer;
 export default function useObserver(elem: String, callback: Function, watch: []) {
   useEffect(() => {
+    console.log('enter page')
     const elements = document.querySelector(elem)
     observer = new IntersectionObserver(
       entries => {
         console.log(`entries`, entries)
       }
     )
-    observer.unobserve(elements)
+    observer.observe(elements)
     return () => {
+      console.log('leave page')
       if(observer){
         observer.unobserve(elements)
-        observer.unobserve()
+        observer.disconnect()
       }
     }
   }, watch)
--- frontend/src/pages/home/index.tsx
+++ frontend/src/pages/home/index.tsx
@@ -1,22 +1,34 @@
 import React, {useState, useEffect} from 'react';
 import { BsFillAlarmFill } from "react-icons/bs";
 import { SearchBar, Button, WhiteSpace, WingBlank } from 'antd-mobile';
-
+import { useObserver } from '@/hooks/index'
+import { history } from "umi";
+import './index.less'
 interface propsType {
   a: string
 }
 
 export default function (props: propsType) {
   const [state, setState ] = useState()
-  useEffect(() => {
+  useObserver('.observable', (entries:Object)=> {
+    console.log(`entries`, entries)
+  }, [])
+  const handleClick = () => {
+    history.push('/order')
+  }
+  
 
-  },[])
   return (
     <div>
       <WhiteSpace />
       <BsFillAlarmFill style={{ fontSize: '4em'}}/>Home Page
       <WhiteSpace />
       <SearchBar placeholder="Search" maxLength={8} />
+      <Button onClick={handleClick} type='primary'>redirect to Order Page</Button>
+      <div className='observable'>
+        observable
+      </div>
+      <br />
     </div>
   )
 }
